f2ed060e26d49592df9b79113b66a9faeaa765df
Raouf Madani <madaniraouff@gmail.com>
2023-09-03 13:34:50 -0400

    update new cv 2023

--- components/About.jsx
+++ components/About.jsx
@@ -1,45 +1,65 @@
-import React from 'react';
-import '../styles/about.css';
-import raoufimage from '../imagescv/newraoufimage.jpg';
-import linkedin from '../imagescv/linkedin.png';
-import github from '../imagescv/github.png';
-import cvfile from '../files/Abderraouf_Madani_CV.pdf';
+import React from "react";
+import "../styles/about.css";
+import raoufimage from "../imagescv/newraoufimage.jpg";
+import linkedin from "../imagescv/linkedin.png";
+import github from "../imagescv/github.png";
+import cvfile from "../files/Abderraouf_Madani_CV.pdf";
 
 const About = () => {
-    const and = '&';
-    return (
-       
-        <section id="about" className="container-fluid">
-        
-            <div className="col-xs-12 col-md-6 col-lg-6 profile-picture">
-                <img src={raoufimage} alt="Raouf" className="img-circle" style={{"backgroundColor":"blue"}} /> 
-                <div className="heading">
-                    <h1>Hello it's me Raouf</h1>
-                </div>
+   return (
+      <section id="about" className="container-fluid">
+         <div className="col-xs-12 col-md-6 col-lg-6 profile-picture">
+            <img src={raoufimage} alt="Raouf" className="img-circle" style={{ "backgroundColor": "blue" }} />
+            <div className="heading">
+               <h1>Hello it's me Raouf</h1>
             </div>
-                <div className="col-xs-12 col-md-6 col-lg-6 info">
-                <div className="heading">
-                    <div id="intro">
-                        <h3>Information</h3>
-                        <div className="white-divider"></div>
-                        <br/>
-                        <p>Full Name: Ahmed Abderraouf MADANI</p>
-                        <p style={{"color":"#fff"}}>Javascript Full-Stack developer</p>
-                        <p>Recently: Winner of Algeria Startup Challenge 2020</p>
-                        <p>Diploma: Master Degree in Software Engineering</p> 
-                        <p>Date {and} Place of Birth: 20/09/1995 Blida, Algeria</p>
-                        <p>Marital status: Married</p>
-                        <p>Whatsapp: +213540757115</p>
-                        <p>Email: raoufmadani@ayoungleader.com</p>
-                        <a href="https://www.linkedin.com/in/raoufmadani/" target="_blank"><img src={linkedin} /></a> <a href="https://github.com/raouf-madani/" target="_blank"><img src={github} /></a><br/>
-                        <a href={cvfile} className="button1">Download CV</a>
-                    </div>         
-                </div>                
-           </div>
-       </section>
-        
-    );
-
+         </div>
+         <div className="col-xs-12 col-md-6 col-lg-6 info">
+            <div className="heading">
+               <div id="intro">
+                  <h3>Information</h3>
+                  <div className="white-divider"></div>
+                  <br />
+                  <p>
+                     Software developer at{" "}
+                     <a href="https://chronoinnovation.com" target="_blank">
+                        Chrono Innovation
+                     </a>
+                  </p>
+                  <p>
+                     Consultant full-stack developer at{" "}
+                     <a href="https://garda.com" target="_blank">
+                        Garda World
+                     </a>
+                  </p>
+                  <p>
+                     Winner of{" "}
+                     <a
+                        href="https://www.linkedin.com/feed/update/urn:li:activity:6765546695582388224/"
+                        target="_blank"
+                     >
+                        Algeria Startup Challenge 2020
+                     </a>
+                  </p>
+                  <p>27yo, Master Degree in Software Engineering</p>
+                  <p>Montreal, Quebec, Canada</p>
+                  <p>Whatsapp: +15142625830</p>
+                  <p>Email: raoufmadani@live.fr</p>
+                  <a href="https://www.linkedin.com/in/raoufmadani/" target="_blank">
+                     <img src={linkedin} />
+                  </a>{" "}
+                  <a href="https://github.com/raoufmadani/" target="_blank">
+                     <img src={github} />
+                  </a>
+                  <br />
+                  <a href={cvfile} className="button1">
+                     Download CV
+                  </a>
+               </div>
+            </div>
+         </div>
+      </section>
+   );
 };
 
 export default About;
